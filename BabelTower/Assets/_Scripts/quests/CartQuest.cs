8fc72dcd69d722739531b0fb0b07ecd172f2076e
Adam <adam.butakov@yandex.ru>
2023-04-23 20:56:15 +0300

    УРАААА

---
+++ BabelTower/Assets/_Scripts/quests/CartQuest.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartQuest : MonoBehaviour
+{
+    public QuestManager manager;
+    public Inventory inventory;
+    public bool cursorOnObject;
+    private void OnMouseEnter()
+    {
+        cursorOnObject = true;
+    }
+    private void OnMouseExit()
+    {
+        cursorOnObject = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                //Debug.Log("Е нажата");
+                if (FindItem(inventory.currentItems, manager.stickOnFire) == true)
+                {
+                    //Debug.Log("Я люблю пицуцу");
+                    manager.isCartInBurn = true;
+                }
+            }
+        }
+    }
+
+    public bool FindItem(List<Items> list, Items item)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] == item)
+                return true;
+        }
+        return false;
+    }
+    public int FindItemInt(List<Items> list, Items item)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] == item)
+                return i;
+        }
+        return 0;
+    }
+}
